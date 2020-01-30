583bde78cbd411105f8f10027f0f9dd487a44cb8
matt day <mattdxc982@gmail.com>
2020-01-30 02:37:05 +0000

    database

---
+++ db/migrate/20200130023302_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.string :summary
+      t.string :game
+      t.timestamps
+    end
+  end
+end

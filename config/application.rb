ebf2d2eca23f89de594b2cc594895d3f2e8ed67a
matt day <mattdxc982@gmail.com>
2020-02-01 05:10:44 +0000

    fonts

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Ffseries
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
